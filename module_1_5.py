0c022d7d6011ddb39a06d469bc2648ba38750db8
filossoof <filossoof@yandex.ru>
2025-01-06 21:51:17 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,16 @@
+immutable_var = tuple(["тип", 0.4, 6.3, "напряжение", "ток"])
+print(immutable_var)
+immutable_var += (" номер",) # если не ставить запятую, то получится строка а не кортеж
+print(immutable_var)
+
+print(immutable_var[2])
+# print(immutable_var[2]) = 10  можно получить элемент по индексу, но из менить его нельзя.
+# кортеж относится к неизменяемым типам.
+
+mutable_list = ["тип", "напряжение", "ток"]
+print(mutable_list)
+mutable_list.append(" номер")
+print(mutable_list)
+print(mutable_list[::-1])
+mutable_list.remove("ток")
+print(mutable_list)
